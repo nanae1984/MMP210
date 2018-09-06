7020c160680820e22eecbc4a1129f199720ab596
nanae1984 <nanaeitoi@gmail.com>
2018-09-06 15:21:00 -0400

    organize

--- self-portrait/sketch.js
+++ self-portrait/sketch.js
@@ -1,8 +1,24 @@
+/*
+    Self Portrait
+    v1
+    by nanae
+    mmp 210 exple week2
+*/
+
 function setup() {
 	createCanvas(640, 360);
 }
 
 function draw() {
 	background(220);
-	ellipse(320, 180, 100);
+    
+    // head
+    rect(100,100,200,250);
+    
+    // eyes
+	ellipse(280, 180, 100);
+    ellipse(120, 180, 100,50);
+    
+    // mouth
+    rect(150,280,100,25,10);
 }
