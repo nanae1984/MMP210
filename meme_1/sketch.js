0ef90829347c15513800b5a2242da6a8c79e6981
nanae1984 <nanaeitoi@gmail.com>
2018-10-11 15:38:54 -0400

    add link

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -10,6 +10,9 @@ var paper; // global
 var brush;
 var paint;
 var paper2;
+var pink;
+var green;
+var blue;
 
 function preload() {
     paper = loadImage("paper.png");
@@ -17,6 +20,9 @@ function preload() {
 
     brush = loadImage("brush.png");
     paint = loadImage("paint.png");
+    pink = loadImage("pink.png");
+    green = loadImage("green.png");
+    blue = loadImage("blue.png");
 
 }
 
@@ -30,22 +36,35 @@ function draw() {
     rectMode(CENTER);
 
     noTint();
-    if (mouseX < 200 || mouseX > 400 || mouseY < 200 || mouseY > 400) {
+    if (mouseX < 100 || mouseX > 400 || mouseY < 100 || mouseY > 400) {
         image(paper2, 100, 100, width / 2, height / 2);
     } else {
         image(paper, 100, 100, width / 2, height / 2);
     }
 
+    if (mouseX < width/2 && mouseY < height/2) {
+		// top left
+		image(paint, mouseX/3, mouseY, 200, 200);
+        noTint();
+        image(brush, mouseX / 2, mouseY, 100, 100);
+	} else if (mouseX > width/2 && mouseY < height/2) {
+		// top right
+		image(pink, mouseX/3, mouseY, 200, 200);
+        noTint();
+        image(brush, mouseX / 2, mouseY, 100, 100);
+	} else if (mouseX < width/2 && mouseY > height/2) {
+		// bottom left
+		image(green, mouseX/3, mouseY, 200, 200);
+        noTint();
+        image(brush, mouseX / 2, mouseY, 100, 100);
+	} else {
+		// bottom right
+		image(blue, mouseX/3, mouseY, 200, 200);
+        noTint();
+        image(brush, mouseX / 2, mouseY, 100, 100);
+	}
 
-    var r = 200; // red;
-    var g = map(mouseX, 0, width / 2, 100, 225); // green
-    var b = 0; // blue
-    tint(r, g, b);
-
-    image(paint, mouseX / 3, 189, 200, 200);
-    noTint();
-    image(brush, mouseX / 2, 180, 100, 100);
-    //tint(0, 255, 255);
+    
 
     fill(0);
     stroke('#FF006C');
@@ -59,5 +78,19 @@ function draw() {
     var length = str.length;
     var n = map(mouseX, 400, 180, 0, length);
     var displayString = str.substring(0, n);
-    text(displayString, 230, 220)
+    text(displayString, 230, 220);
+    
+    
+    // link
+    stroke('white'); noFill();
+    rectMode(CORNER);
+    rect(200, 200, 200, 100);
+    if (mouseIsPressed && mouseX > 200 && mouseX < 200 + 200
+       && mouseY > 200 && mouseY < 200 + 100) {
+        location.href = '../'
+    }
+    
+    if (keyIsPressed && keyCode == 32) {
+        location.href = '../'
+    }
 }
