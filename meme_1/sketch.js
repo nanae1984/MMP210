b4eab17760c1cd4a828366f527a9b552d03f38a6
nanae1984 <nanaeitoi@gmail.com>
2018-10-14 23:13:39 -0400

    add images

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -6,30 +6,36 @@
     mmp 210 week 6
  */
 
-var paper; // global
+//var paper; // global
 var brush;
 //var paint;
-var paper2;
+//var paper2;
 var pink;
 var green;
 //var blue;
 var brain;
-var math;
-var math2;
-var pencil;
+var cube;
+var word;
+var word2;
+var creative;
+var logical;
+var keyIsPressed;
 
 function preload() {
-    paper = loadImage("paper.png");
-    paper2 = loadImage("paper2.png");
+   // paper = loadImage("paper.png");
+   // paper2 = loadImage("paper2.png");
     brain = loadImage("brain.png");
     brush = loadImage("brush.png");
     //paint = loadImage("paint.png");
     pink = loadImage("pink.png");
-    green = loadImage("green.png");
+   // green = loadImage("green.png");
     //blue = loadImage("blue.png");
-    math = loadImage("math.png");
-    math2 = loadImage("math2.png");
-    pencil = loadImage("pencil.png");
+    cube = loadImage("cube.png");
+    word = loadImage("word.png");
+    word2 = loadImage("word2.png")
+    creative = loadImage("creative.jpeg");
+    logical = loadImage("logical.jpg");
+    
 
 
 }
@@ -50,29 +56,32 @@ function draw() {
    // } else {
       //  image(paper, 100, 100, width / 2, height / 2);
    // }
+if (mouseX > width/2){
+    //right
+    image(creative, width/2, 0, width/2, height );
 
+} else {
+    //left
+     image(logical, 0, 0, width/2, height );
+}
+    
     if (mouseX < width/2 && mouseY < height/2) {
 		// top left
-		image(math2, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(pencil, mouseX / 2, mouseY, 100, 100);
+		image(word, mouseX/3, mouseY, 250, 250);
+       
 	} else if (mouseX > width/2 && mouseY < height/2) {
 		// top right
 		image(pink, mouseX/3, mouseY, 200, 200);
-        noTint();
         image(brush, mouseX / 2, mouseY, 100, 100);
 	} else if (mouseX < width/2 && mouseY > height/2) {
 		// bottom left
-		image(math, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(pencil, mouseX / 2, mouseY, 100, 100);
+		image(cube, mouseX/3, mouseY, 250, 250);
+       
 	} else {
 		// bottom right
-		image(green, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(brush, mouseX / 2, mouseY, 100, 100);
+		image(word2, mouseX/3, mouseY, 250, 250);
+        
 	}
-
     
 
     fill(0);
@@ -89,6 +98,7 @@ function draw() {
     var displayString = str.substring(0, n);
     text(displayString, 230, 220);
     
+     
     
     // link
     stroke(245,250,255,0.5); noFill();
@@ -100,6 +110,7 @@ function draw() {
     }
     
     if (keyIsPressed && keyCode == 32) {
-        location.href = '../'
+        location.href = '../https://nanae1984.github.io/MMP210/meme_1_2/'
     }
 }
+   
--- meme_1_2/sketch.js
+++ meme_1_2/sketch.js
@@ -7,88 +7,50 @@
  */
 
 var paper; // global
-var brush;
+var gif;
 //var paint;
 var paper2;
-var pink;
-var green;
-//var blue;
-var brain;
-var math;
-var math2;
-var pencil;
+
 
 function preload() {
     paper = loadImage("paper.png");
     paper2 = loadImage("paper2.png");
-    brain = loadImage("brain.png");
-    brush = loadImage("brush.png");
-    //paint = loadImage("paint.png");
-    pink = loadImage("pink.png");
-    green = loadImage("green.png");
-    //blue = loadImage("blue.png");
-    math = loadImage("math.png");
-    math2 = loadImage("math2.png");
-    pencil = loadImage("pencil.png");
-
+   
 
 }
 
 function setup() {
     createCanvas(500, 500);
-
+    gif = loadGif('bunny.gif');
 }
 
 function draw() {
     background(220);
     rectMode(CENTER);
+    image(gif, 0, 0);
     
-    noTint();
-    //image(brain,0,0,width,height);
+   // mouse funtion
     if (mouseX < 100 || mouseX > 400 || mouseY < 100 || mouseY > 400) {
        image(paper2, 100, 100, width / 2, height / 2);
    } else {
         image(paper, 100, 100, width / 2, height / 2);
     }
 
-    if (mouseX < width/2 && mouseY < height/2) {
-		// top left
-		image(math2, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(pencil, mouseX / 2, mouseY, 100, 100);
-	} else if (mouseX > width/2 && mouseY < height/2) {
-		// top right
-		image(pink, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(brush, mouseX / 2, mouseY, 100, 100);
-	} else if (mouseX < width/2 && mouseY > height/2) {
-		// bottom left
-		image(math, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(pencil, mouseX / 2, mouseY, 100, 100);
-	} else {
-		// bottom right
-		image(green, mouseX/3, mouseY, 200, 200);
-        noTint();
-        image(brush, mouseX / 2, mouseY, 100, 100);
-	}
+  function mouseMoved() {
+  if (gif.loaded() && !gif.playing()){
+    var totalFrames = gif.totalFrames();
+    var frame = int(map(mouseX, 0, width, 0, totalFrames));
+    gif.frame(frame);
+  }
+}
+ function mousePressed() {
+  if (gif.playing()) {
+    gif.pause();
+  } else {
+    gif.play();
+  }
+} 
 
-    
-
-    fill(0);
-    stroke('#FF006C');
-    strokeWeight(3);
-    textAlign(CENTER, CENTER);
-    textSize(100);
-    textFont("Amatic SC, cursive");
-    //text("Enter", 230, 220);
-
-    var str = "Enter";
-    var length = str.length;
-    var n = map(mouseX, 400, 180, 0, length);
-    var displayString = str.substring(0, n);
-    text(displayString, 230, 220);
-    
     
     // link
     stroke(245,250,255,0.5); noFill();
